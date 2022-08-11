480ba071f280df3dd8580d7c3252987d940dbb89
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-08-11 12:22:09 +0530

    download mail attachments
    Once you have credentials.json, the first time you run the script it will open up a browser window asking you to log in to your Gmail account and allow "Quickstart" app to access it. A token.json file will be generated which your script can use to access your account.

---
+++ Attachment Downloader.py
@@ -0,0 +1,34 @@
+import ezgmail
+def attachmentdownload(resulthreads):
+    countofresults = len(resulthreads)
+    try:
+        for i in range(countofresults):
+            if len(resulthreads[i].messages) > 1:
+                for j in range(len(resulthreads[i].messages)):
+                    resulthreads[i].messages[
+                        j].downloadAllAttachments()
+            else:
+                resulthreads[i].messages[0].downloadAllAttachments()
+        print("Download compelete. Please check your root directory.")
+    except:
+        raise Exception("Error occured while downloading attachment(s).")
+
+if __name__ == '__main__':
+    query = input("Enter search query: ")
+    newquery = query + " + has:attachment"
+    resulthreads = ezgmail.search(newquery)
+    if len(resulthreads) == 0:
+        print("Result has no attachments:")
+    else:
+        print("Result(s) with attachments:")
+        for threads in resulthreads:
+            print(f"Email Subject: {threads.messages[0].subject}")
+        try:
+            ask = input(
+                "Do you want to download attachment(s) in result(s) (Yes/No)? ")
+            if ask == "Yes":
+                attachmentdownload(resulthreads)
+            else:
+                print("Program exited")
+        except:
+            print("Something went wrong")
