f95d29c57a8927b10ed935390e1011d870e885dc
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-08-24 23:44:33 +0530

    automate login to facebook

---
+++ Facebook login.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+user_id=input('Enter User Id of your Fb Account :')
+password=input('Enter the password :')
+print(user_id)
+print(password)
+cd='C:\\webdrivers\\chromedriver.exe'
+browser= webdriver.Chrome(cd)
+browser.get('https://www.facebook.com/')
+user_box = browser.find_element_by_id("email")
+user_box.send_keys(user_id)
+password_box = browser.find_element_by_id("pass")
+password_box.send_keys(password)
+login_box = browser.find_element_by_id("u_0_b")
+login_box.click()
