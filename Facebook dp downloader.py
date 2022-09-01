06d3daec4d7bcf16d64b47fabb742677c769b05d
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-09-02 00:03:19 +0530

    download display pics from facebook id

---
+++ Facebook dp downloader.py
@@ -0,0 +1,13 @@
+import os
+import requests
+url="https://graph.facebook.com/{}/picture?type=large"
+path = os.getcwd()
+if not "fb_dps" in os.listdir(path):
+    os.mkdir("fb_dps")
+fbid=int(input("Enter the Facebook-id to download it's profile picture: "))
+try:
+    result=requests.get(url.format(fbid))
+    with open("fb_dps/{}_img.jpg".format(fbid),"wb") as file:
+        file.write(result.content)
+except:
+	print("There was some error")
