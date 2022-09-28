39e7a9edc3153aa76ae39b59841b079e698356ee
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-09-28 23:54:02 +0530

    apply watermark on images

---
+++ Watermark Images.py
@@ -0,0 +1,32 @@
+import os
+from PIL import Image
+from PIL import ImageFilter
+def watermark_photo(input_image_path, output_image_path, watermark_image_path):
+    base_image = Image.open(input_image_path)
+    watermark = Image.open(watermark_image_path)
+    position = base_image.size
+    watermark.size
+    newsize = int(position[0] * 8 / 100), int(position[0] * 8 / 100)
+    watermark = watermark.resize(newsize)
+    new_position = position[0] - newsize[0] - 20, position[1] - newsize[1] - 20
+    transparent = Image.new(mode="RGBA", size=position, color=(0, 0, 0, 0))
+    transparent.paste(base_image, (0, 0))
+    transparent.paste(watermark, new_position, mask=watermark)
+    image_mode = base_image.mode
+    if image_mode == "RGB":
+        transparent = transparent.convert(image_mode)
+    else:
+        transparent = transparent.convert("P")
+    transparent.save(output_image_path, optimize=True, quality=100)
+    print("Saving " + output_image_path + " ...")
+folder = input("Enter Folder Path : ")
+watermark = input("Enter Watermark Path : ")
+os.chdir(folder)
+files = os.listdir(os.getcwd())
+if not os.path.isdir("output"):
+    os.mkdir("output")
+c = 1
+for f in files:
+    if os.path.isfile(os.path.abspath(f)):
+        if f.endswith(".png") or f.endswith(".jpg"):
+            watermark_photo(f, "output/" + f, watermark)
