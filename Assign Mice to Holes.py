2bc936a9eaf1cd438186c2c9e764cd2e8d3d5e23
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-12-08 23:42:38 +0530

    Assign Mice to Holes greedy problem

---
+++ Assign Mice to Holes.py
@@ -0,0 +1,40 @@
+def assignHole(mices, holes, n, m):
+	
+	# Base Condition
+	# No. of mouse and holes should be same
+	if (n != m):
+		return -1
+	
+	# Sort the arrays
+	mices.sort()
+	holes.sort()
+	
+	# Finding max difference between
+	# ith mice and hole
+	Max = 0
+	
+	for i in range(n):
+		if (Max < abs(mices[i] - holes[i])):
+			Max = abs(mices[i] - holes[i])
+	
+	return Max
+	
+# Driver code
+
+# Position of mouses
+mices = [ 4, -4, 2 ]
+
+# Position of holes
+holes = [ 4, 0, 5 ]
+
+# Number of mouses
+n = len(mices)
+
+# Number of holes
+m = len(holes)
+
+# The required answer is returned
+# from the function
+minTime = assignHole(mices, holes, n, m)
+
+print("The last mouse gets into the hole in time:", minTime)
